426b8c0a1475f3935b90a8cbca190224d53e16d7
Huy Tu <dragon10x0408@gmail.com>
2020-12-08 02:56:41 +0700

    change logo + add .env cookie secret string

--- app.js
+++ app.js
@@ -32,7 +32,7 @@ app.set('view engine', 'hbs');
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
-app.use(cookieParser('MY SECRET'));
+app.use(cookieParser(process.env.SESSION_SECRET));
 app.use(express.static(path.join(__dirname, 'public')));
 
 
